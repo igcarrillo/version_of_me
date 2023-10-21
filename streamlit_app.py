0faee25b465e68d0607e434fa7f65ccae92b3f1e
Ignacio Garcia-Carrillo <ignaciogcarrillo@gmail.com>
2023-10-20 21:43:39 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,7 +11,7 @@ st.title("Chat with " + st.secrets.character + ", powered by LlamaIndex 💬")
 if "messages" not in st.session_state.keys(): # Initialize the chat messages history
     st.session_state.messages = [
         {"role": "assistant", "content": "Please ask me a question about " + st.secrets.character },
-         {"role": "system", "content": "You are an assistant that responds and speaks like " + st.secrets.character }) # add context to the response
+         {"role": "system", "content": "You are an assistant that responds and speaks like " + st.secrets.character } # add context to the response
     ]
 
 @st.cache_resource(show_spinner=False)
